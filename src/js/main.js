13af151178575701ff45e16e686940be01472f73
YutaNakajima <yuta.nakajima@glucose.jp>
2017-10-16 17:01:11 +0900

    2017/10/16 17:00

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 // Component
 const Title = (props) => {
-  return <h1 className="calculator-title">{props.title}</h1>
-}
+  return <h1 className="calculator-title">{props.title}</h1>;
+};
 
 const Display = (props) => {
-  return <p className="display">{props.number}</p>
-}
+  return <p className="display">{props.number}</p>;
+};
 
 const Button = (props) => (
-  <button className={`btn btn-${props.type}`} type="button" onClick={() => props.onButtonTouch(props.symbol)}>{props.symbol}</button>
-)
+  <button
+    className={`btn btn-${props.type}`}
+    type="button"
+    onClick={() => props.onButtonTouch(props.symbol)}>{props.symbol}</button>
+);
+
 
 class Panel extends React.Component {
   constructor(props) {
     super(props);
   }
   renderButtons(type, symbol) {
-    return <td><Button type={type} symbol={symbol} onButtonTouch={this.props.onButtonTouch} /></td>
+    return <td><Button
+    type={type}
+    symbol={symbol}
+    onButtonTouch={this.props.onButtonTouch} /></td>;
   }
   renderZeroButton() {
-    return <td colSpan="2"><Button type="default btn-zero" symbol={0} onButtonTouch={this.props.onButtonTouch} /></td>
+    return <td colSpan="2"><Button
+      type="default btn-zero"
+      symbol={0}
+      onButtonTouch={this.props.onButtonTouch} /></td>;
   }
   render() {
-    return(
+    return (
       <table>
         <tbody>
           <tr>
@@ -64,7 +73,6 @@ class Panel extends React.Component {
   }
 }
 
-
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
@@ -73,73 +81,89 @@ class Calculator extends React.Component {
       operator: null,
       first_number: 0,
       second_number: 0,
-      display_number: 0
+      display_number: 0,
     };
-    this.onButtonTouch = this.onButtonTouch.bind(this);
+    this.onButtonTouch = this
+      .onButtonTouch
+      .bind(this);
   }
   onButtonTouch(_button) {
     // 何れかのボンタンが押された時に呼ばれる
 
-    if(isNaN(_button)) {
-      // 数値以外のボタンが押された場合
+    if (isNaN(_button)) {
+      // 数値以外のボタンが押された場合 SAVE the World
+      this.state.symbol = _button;
 
-      // SAVE the World
-      this.state.symbol = _button
-
-      if(this.state.symbol !== '=') {
+      if (this.state.symbol !== '=') {
         // = 以外を operator として保存
         this.state.operator = this.state.symbol;
       }
 
-      if(_button === '=') {
+      if (_button === '=') {
         // = 押したとき計算
-        this.state.first_number = calc(this.state.operator, this.state.first_number, this.state.second_number)
+        this.state.first_number = calc(
+          this.state.operator,
+          this.state.first_number,
+          this.state.second_number
+        );
         this.setState(() => {
           return {
             // 続けて計算を行えるように、計算結果を最初の数値に保存し二番目の数値を初期化
             display_number: Number(this.state.first_number),
-            second_number: 0
+            second_number: 0,
           };
         });
       }
 
-      if(_button === 'AC') {
+      if (_button === 'AC') {
         // AC押したら初期化
         this.setState(() => {
-          return {
-            symbol: null,
+          return {symbol: null,
             first_number: 0,
             second_number: 0,
-            display_number: 0
+            display_number: 0,
           };
         });
       }
-
     } else {
       // 数値ボタンが押されたの場合
 
-      if(!this.state.symbol) {
+      if (!this.state.symbol) {
         // 記号が入力される前
-        if(this.state.first_number === 0) {
+        if (this.state.first_number === 0) {
           this.setState(() => {
             // 最初だけ押されたボタンの数値を入れる
-            return {display_number: this.state.first_number = _button};
+            return {
+              display_number: this.state.first_number = _button,
+            };
           });
         } else {
           this.setState(() => {
             // ディスプレイに表示されている数値 + 押されたボタンの数値の文字列を数値にキャストしてディスプレイに表示する
-            return {display_number: Number(this.state.first_number = '' + this.state.first_number + _button)};
+            return {
+              display_number: Number(
+                this.state.first_number = '' +
+                this.state.first_number + _button
+              ),
+            };
           });
         }
       } else {
         // 記号が入力された後
-        if(this.state.second_number === 0) {
+        if (this.state.second_number === 0) {
           this.setState(() => {
-            return {display_number: this.state.second_number = _button};
+            return {
+              display_number: this.state.second_number = _button,
+            };
           });
         } else {
           this.setState(() => {
-            return {display_number: Number(this.state.second_number = '' + this.state.second_number + _button)};
+            return {
+              display_number: Number(
+                this.state.second_number = '' +
+                this.state.second_number + _button
+              ),
+            };
           });
         }
       }
@@ -150,15 +174,14 @@ class Calculator extends React.Component {
       this.state.first_number,
       this.state.second_number,
       this.state.symbol
-    )
-
+    );
   }
   render() {
     return (
       <div>
-        <Title title="React Calculator" />
-        <Display number={this.state.display_number} />
-        <Panel onButtonTouch={this.onButtonTouch} />
+        <Title title="React Calculator"/>
+        <Display number={this.state.display_number}/>
+        <Panel onButtonTouch={this.onButtonTouch}/>
       </div>
     );
   }
@@ -166,16 +189,13 @@ class Calculator extends React.Component {
 
 // Renderer
 ReactDOM.render(
-  <Calculator />,
-  document.getElementById('app')
-);
-
+  <Calculator/>, document.getElementById('app'));
 
 // Functions
 function calc(operator, x, y) {
   let ans = 0;
 
-  switch (operator){
+  switch (operator) {
     case '+':
       ans = Number(x) + Number(y);
       break;
@@ -193,5 +213,5 @@ function calc(operator, x, y) {
       break;
   }
 
-  return ans
+  return ans;
 }
